be8c9285f8318aa7de1ae180a13be830e414a7d3
JuliaKirillova <panic_motion@mail.ru>
2021-11-12 17:00:52 +0300

    Add product to basket test

--- pages/locators.py
+++ pages/locators.py
@@ -3,8 +3,16 @@ from selenium.webdriver.common.by import By
 
 class MainPageLocators:
     LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
-
+    # LOGIN_LINK = (By.ID, "#login_link")
 
 class LoginPageLocators:
+    # LOGIN_URL = (By.ID, "/en-gb/accounts/login/")
     LOGIN_FORM = (By.ID, "login_form")
     REGISTER_FORM = (By.ID, "register_form")
+
+class AddToBasketLocators:
+    ADD_TO_BASKET = (By.CLASS_NAME, "btn-add-to-basket")
+    BOOK_NAME = (By.TAG_NAME, "h1")
+    MESSAGE_BOOK_NAME = (By.CSS_SELECTOR, "div.alertinner > strong")
+    BOOK_PRICE= (By.CSS_SELECTOR, "div.product_main > p.price_color")
+    MESSAGE_BOOK_PRICE = (By.CSS_SELECTOR, "div.alert-info p strong")
---
+++ pages/product_page.py
@@ -0,0 +1,52 @@
+from selenium.common.exceptions import NoAlertPresentException # в начале файла
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from base_page import BasePage
+from locators import AddToBasketLocators
+import math
+import time
+
+class ProductPage(BasePage):
+    def add_product_to_basket_and_check_messages(self):
+        self.add_to_basket()
+        self.solve_quiz_and_get_code()
+        self.should_be_message_add_product_to_basket()
+        self.should_be_message_price_product_to_basket()
+
+    def add_to_basket(self):
+        # Добавляем товар в корзину
+        link = self.browser.find_element(*AddToBasketLocators.ADD_TO_BASKET)
+        link.click()
+
+    def solve_quiz_and_get_code(self):
+        # Заполняем проверочный код для всплывающего окна
+        WebDriverWait(self.browser, 10).until(EC.alert_is_present())
+        alert = self.browser.switch_to.alert
+        x = alert.text.split(" ")[2]
+        answer = str(math.log(abs((12 * math.sin(float(x))))))
+        alert.send_keys(answer)
+        alert.accept()
+        try:
+            alert = self.browser.switch_to.alert
+            alert_text = alert.text
+            print(f"Your code: {alert_text}")
+            time.sleep(20)
+            alert.accept()
+        except NoAlertPresentException:
+            print("No second alert presented")
+
+    def should_be_message_add_product_to_basket(self):
+        # Проверяем сообщение о том, что товар добавлен в корзину
+        message_book_name = self.browser.find_element(*AddToBasketLocators.MESSAGE_BOOK_NAME)
+        print(message_book_name.text)
+        book_name = self.browser.find_element(*AddToBasketLocators.BOOK_NAME)
+        print(book_name.text)
+        assert message_book_name.text == book_name.text, "Not the same book"
+
+    def should_be_message_price_product_to_basket(self):
+        # Проверяем сообщение о цене товара после добавления в корзину
+        book_price = self.browser.find_element(*AddToBasketLocators.BOOK_PRICE)
+        print(book_price.text)
+        message_book_price = self.browser.find_element(*AddToBasketLocators.MESSAGE_BOOK_PRICE)
+        print(message_book_price.text)
+        assert book_price.text  == message_book_price.text, "Not the same price"
