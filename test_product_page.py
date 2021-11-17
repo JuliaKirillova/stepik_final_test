497eb60a6ecbc73ade8b50009f42033de023ce08
JuliaKirillova <panic_motion@mail.ru>
2021-11-17 13:56:29 +0300

    Add transitions from product page to login page

--- test_product_page.py
+++ test_product_page.py
@@ -1,18 +1,50 @@
 from product_page import ProductPage
 import pytest
 
-
-@pytest.mark.parametrize('offer_number', ["0","1","2","3","4","5","6",pytest.param("7", marks=pytest.mark.xfail),"8","9"])
-
-
+# Тест на параметризацию
+@pytest.mark.parametrize('offer_number',
+                         ["0", "1", "2", "3", "4", "5", "6", pytest.param("7", marks=pytest.mark.xfail), "8", "9"])
 def test_guest_can_add_product_to_basket(browser, offer_number):
-    # link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear"
     link = f"http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer{offer_number}"
     page = ProductPage(browser, link)
     page.open()
     page.add_product_to_basket_and_check_messages()
-    # Каждая проверка по отдельности ниже
-    # page.add_to_basket()
-    # page.solve_quiz_and_get_code()
-    # page.should_be_message_add_product_to_basket()
-    # page.should_be_message_price_product_to_basket()
+
+# Проверка отсутсвия сообщения об успехе
+def test_guest_cant_see_success_message_after_adding_product_to_basket(browser):
+    link = f"http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer9"
+    page = ProductPage(browser, link)
+    page.open()  # Открываем страницу товара
+    page.add_to_basket()  # Добавляем товар в корзину
+    page.solve_quiz_and_get_code()
+    page.should_not_be_success_message()  # Проверяем, что нет сообщения об успехе с помощью is_not_element_present
+
+# Проверка отсутсвия сообщения об успехе
+def test_guest_cant_see_success_message(browser):
+    link = f"http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer9"
+    page = ProductPage(browser, link)
+    page.open()  # Открываем страницу товара
+    page.should_not_be_success_message()  # Проверяем, что нет сообщения об успехе с помощью is_not_element_present
+
+# Проверка исчезающего сообщения об успехе
+def test_message_disappeared_after_adding_product_to_basket(browser):
+    link = f"http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer9"
+    page = ProductPage(browser, link)
+    page.open()  # Открываем страницу товара
+    page.add_to_basket()  # Добавляем товар в корзину
+    page.solve_quiz_and_get_code()
+    page.should_be_disappear_success_message()  # Проверяем, что сообщение об успехе исчезает с помощью is_disappeared
+
+# Проверка отображения ссылки на страницу логина со страницы продукта
+def test_guest_should_see_login_link_on_product_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+    page = ProductPage(browser, link)
+    page.open()
+    page.should_be_login_link()
+
+# Проверка перехода на страницу логина со страницы продукта
+def test_guest_can_go_to_login_page_from_product_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+    page = ProductPage(browser, link)
+    page.open()
+    page.go_to_login_page()
