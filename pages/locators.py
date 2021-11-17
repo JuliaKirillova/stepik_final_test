dd8d9269199f57cd75939a9000be4186debc2cb6
JuliaKirillova <panic_motion@mail.ru>
2021-11-17 13:58:45 +0300

    Add transitions from product page to login page

--- pages/locators.py
+++ pages/locators.py
@@ -1,17 +1,19 @@
 from selenium.webdriver.common.by import By
 
-
-class MainPageLocators:
+class BasePageLocators():
     LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
-    # LOGIN_LINK = (By.ID, "#login_link")
+    LOGIN_LINK_INVALID = (By.CSS_SELECTOR, "#login_link_inc")
 
 class LoginPageLocators:
     # LOGIN_URL = (By.ID, "/en-gb/accounts/login/")
     LOGIN_FORM = (By.ID, "login_form")
     REGISTER_FORM = (By.ID, "register_form")
 
-class AddToBasketLocators:
+class ProductPageLocators:
     ADD_TO_BASKET = (By.CLASS_NAME, "btn-add-to-basket")
+    # SEE_THE_BASKET = (By.ID, "/ru/basket/")
+    # SUCCESS_MESSAGE = (By.ID, "messages")
+    SUCCESS_MESSAGE = (By.CSS_SELECTOR, "div.alertinner > strong")
     BOOK_NAME = (By.TAG_NAME, "h1")
     MESSAGE_BOOK_NAME = (By.CSS_SELECTOR, "div.alertinner > strong")
     BOOK_PRICE= (By.CSS_SELECTOR, "div.product_main > p.price_color")
--- pages/main_page.py
+++ pages/main_page.py
@@ -1,8 +1,18 @@
 from base_page import BasePage
-from locators import MainPageLocators
+# from locators import MainPageLocators
 
 
 class MainPage(BasePage):
-    def go_to_login_page(self):
-        link = self.browser.find_element(*MainPageLocators.LOGIN_LINK)
-        link.click()
+    def __init__(self, *args, **kwargs):
+        super(MainPage, self).__init__(*args, **kwargs)
+
+# До переноса элементов в BasePage https://stepik.org/lesson/201964/step/8?unit=176022 см. ниже
+# class MainPage(BasePage):
+#     def go_to_login_page(self):
+#         link = self.browser.find_element(*MainPageLocators.LOGIN_LINK)
+#         link.click()
+
+
+
+
+
