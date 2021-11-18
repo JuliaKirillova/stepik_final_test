0d35b4aa495890d8ad137dc2d38f62144b3d0163
JuliaKirillova <panic_motion@mail.ru>
2021-11-18 19:03:42 +0300

    Delete basket_page.py

--- pages/basket_page.py
+++
@@ -1,9 +0,0 @@
-from base_page import BasePage
-from locators import BasketPageLocators
-
-class BasketPage(BasePage):
-    def check_product_in_basket(self):
-        assert self.is_not_element_present(*BasketPageLocators.CHECK_NO_PRODUCT_IN_BASKET), "There is some product in the basket"
-
-    def check_basket_is_empty_message(self):
-        assert self.is_element_present(*BasketPageLocators.EMPTY_BASKET_MESSAGE), "There is no message that basket is empty"
